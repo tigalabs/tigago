a3d7ad02ee2cbe8ef05f690fdaaefc4060d00d7c
misitebao <i@misitebao.com>
2021-06-17 12:04:04 +0800

    feat: cleanup module

--- tigago.go
+++ tigago.go
@@ -2,10 +2,3 @@
 //
 //	包 tigago 包是框架的集成包。
 package tigago
-
-import "github.com/tigateam/tigago/modules/net/tghttp"
-
-// NewServer 新建服务
-func NewServer() *tghttp.Server {
-	return tghttp.NewServer()
-}
