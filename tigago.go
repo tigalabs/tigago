43aa4c924755193643493ca1e5b137d060b910ee
misitebao <i@misitebao.com>
2021-08-21 02:39:13 +0800

    feat: perfect annotation

--- tigago.go
+++ tigago.go
@@ -1,4 +1,4 @@
-//	Package tigago is an integration package for the framework.
+// Package tigago is an integration package for the framework.
 //
-//	包 tigago 包是框架的集成包。
+// tigago包是框架的集成包。
 package tigago
